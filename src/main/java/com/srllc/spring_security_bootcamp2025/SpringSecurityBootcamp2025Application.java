a56ebb506a7a1e60109fc8b06bc1ada06f61960c
Alexander Thomas Sayson <alexander@solutionsresource.com>
2025-09-01 10:14:34 +0800

    initial commit

---
+++ src/main/java/com/srllc/spring_security_bootcamp2025/SpringSecurityBootcamp2025Application.java
@@ -0,0 +1,13 @@
+package com.srllc.spring_security_bootcamp2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBootcamp2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBootcamp2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/srllc/spring_security_bootcamp2025/SpringSecurityBootcamp2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srllc.spring_security_bootcamp2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBootcamp2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
